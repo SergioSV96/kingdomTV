22e008d62cd6e8d88bf67333607d41e0a45871f1
MiguelTG35 <migueltg352340@gmail.com>
2018-02-20 18:27:39 +0100

    Profile Controller
    Added profile controller

---
+++ spring/src/main/java/daw/spring/controller/ProfileController.java
@@ -0,0 +1,18 @@
+package daw.spring.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ProfileController {
+
+    @RequestMapping("/profile")
+    public String index(Model model)
+    {
+        model.addAttribute("profileName", "Joe Doe");
+        return "profile";
+    }
+}
