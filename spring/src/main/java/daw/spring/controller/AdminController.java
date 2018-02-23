a987acef272f3f61fe41b5c3ae5bb5bd8d071e6c
MiguelTG35 <migueltg352340@gmail.com>
2018-02-23 19:27:19 +0100

    Admin paths
    Fixed admin path to prevent errors between files

---
+++ spring/src/main/java/daw/spring/controller/AdminController.java
@@ -0,0 +1,15 @@
+package daw.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AdminController
+{
+    @RequestMapping("/admin")
+    public String adminHTML(){
+
+        return "admin";
+    }
+
+}
