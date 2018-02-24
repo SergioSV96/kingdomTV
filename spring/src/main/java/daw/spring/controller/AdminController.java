2ad881d10ecbba14d61678aaf25640d814513e08
MiguelTG35 <migueltg352340@gmail.com>
2018-02-24 19:41:41 +0100

    New admin
    - Added new template for admin.
    - Updated admin controllers.

--- spring/src/main/java/daw/spring/controller/AdminController.java
+++ spring/src/main/java/daw/spring/controller/AdminController.java
@@ -1,15 +1,37 @@
 package daw.spring.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class AdminController
 {
-    @RequestMapping("/admin")
-    public String adminHTML(){
-
-        return "admin";
+    @RequestMapping("/adminMain")
+    public String adminHTML(Model model){
+        model.addAttribute("profileName", "Administrador");
+        model.addAttribute("mailUser", "admin@kingdom.tv");
+        return "adminMain";
     }
 
+    @RequestMapping("/moviesPage")
+    public String moviesPageHTML(Model model){
+        model.addAttribute("profileName", "Administrador");
+        model.addAttribute("mailUser", "admin@kingdom.tv");
+        return "moviesPage";
+    }
+
+    @RequestMapping("/showsPage")
+    public String showsPageHTML(Model model){
+        model.addAttribute("profileName", "Administrador");
+        model.addAttribute("mailUser", "admin@kingdom.tv");
+        return "showsPage";
+    }
+
+    @RequestMapping("/usersPage")
+    public String usersPageHTML(Model model){
+        model.addAttribute("profileName", "Administrador");
+        model.addAttribute("mailUser", "admin@kingdom.tv");
+        return "usersPage";
+    }
 }
