0509868d925b2cecd7567cc3f87bb1b1eb8bcaff
SergioSV96 <serindio@gmail.com>
2018-02-25 01:51:17 +0100

    Added register functionality
    - Still using H2 database for testing

--- spring/src/main/java/daw/spring/controller/RegisterController.java
+++ spring/src/main/java/daw/spring/controller/RegisterController.java
@@ -1,6 +1,15 @@
 package daw.spring.controller;
 
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import daw.spring.entities.User;
+import daw.spring.repositories.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 
@@ -8,12 +17,29 @@ import org.springframework.ui.Model;
 public class RegisterController
 {
 
+	private final UserRepository userRepository;
+	
+	@Autowired
+	public RegisterController(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+	
     @RequestMapping("/register")
     public String registerHTML(Model model){
 
         model.addAttribute("profileName","Prueba de KingomTV");
 
         return "register";
+    }
+    
+    @PostMapping("/register")
+    public String registerUser(@RequestParam String name, @RequestParam String nick,
+    						   @RequestParam String email, @RequestParam String pass){
+       
+    	userRepository.save(new User(name, nick, email, pass, null, "ROLE_USER"));
+    	
+        return "login";
+        
     }
 
 }
