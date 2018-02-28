fc5c9780d82bc4ab982788ae85e26635c6edf09c
MiguelTG35 <migueltg352340@gmail.com>
2018-02-28 20:07:52 +0100

    Revert "Revert "Merge branch 'master' of https://github.com/SergioSV96/kingdomTV""
    This reverts commit ec1c3c618e35eb5801be214f57ad60ecb41e371c.

---
+++ spring/src/main/java/daw/spring/javaclass/MailVerifier.java
@@ -0,0 +1,19 @@
+package daw.spring.javaclass;
+
+import java.util.regex.Pattern;
+
+public class MailVerifier
+{
+    public static boolean isValid(String email)
+    {
+        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\."+
+                "[a-zA-Z0-9_+&*-]+)*@" +
+                "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
+                "A-Z]{2,7}$";
+
+        Pattern pat = Pattern.compile(emailRegex);
+        if (email == null)
+            return false;
+        return pat.matcher(email).matches();
+    }
+}
